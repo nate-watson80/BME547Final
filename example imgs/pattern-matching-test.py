117dc537c8f7a75b5c4c6bb338ed6365d2d406f3
Jason Liu <jl532@duke.edu>
2019-04-13 19:42:40 -0400

    standard image maker made, circle finding algos being tested now...

---
+++ example imgs/pattern-matching-test.py
@@ -0,0 +1,127 @@
+# pattern matching simple
+
+# import libraries 
+import numpy as np 
+import cv2
+import sys
+import csv
+from operator import itemgetter
+
+# for plotting, import these things
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+arrayCoords = []
+def mouseLocationClick(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("click identified at: " +str([x,y]))
+        arrayCoords.append([x,y])
+        
+def pullElementsFromList(datList,argument): # use this when you have a 2d list and want a specific element from each entry
+    return [thatSpecificArgument[argument] for thatSpecificArgument in datList]
+  
+def circleDistanceSorter(circleArray,position,numberofCaptSpots):
+    dist = []
+    for i in circleArray[0,:]: # calculates the distance from each circle to the center of the array
+        distanceFromCenter = np.sqrt( pow((i[0] - position[0]),2) + pow((i[1] - position[1]),2) )
+        dist.append(distanceFromCenter) # stores those values into an array
+    pointers = range(len(circleArray[0,:])) # makes a pointer array that matches the pointers in the "circle" list
+    closestCirclesPointers = sorted(zip(dist,pointers),reverse=False) # sorts and returns the sorted list [distance,pointers]
+    sortedCirclesFromCenter = circleArray[0,pullElementsFromList(closestCirclesPointers,1)] # returns the circle List entries sorted by distance using the pointers to the circle List
+    captureSpots = sortedCirclesFromCenter[:numberofCaptSpots]
+    sortedCaptureSpotsByWhy = sorted(captureSpots, key = itemgetter(1))
+    maxCircleRadius = max(pullElementsFromList(sortedCaptureSpotsByWhy,2))
+    yCoordinateRowOfCircles= sortedCaptureSpotsByWhy[0][1]
+    fullySortedList = []
+    rowCircleList = []
+    for eachCircle in sortedCaptureSpotsByWhy:
+        #print(eachCircle)
+        if (abs(eachCircle[1]-yCoordinateRowOfCircles) < maxCircleRadius):
+            rowCircleList.append(eachCircle)
+            #print(str(eachCircle) + " added")
+        else:
+            rowCirclesSortedByX = sorted(rowCircleList, key = itemgetter(0))
+            fullySortedList = fullySortedList + rowCirclesSortedByX
+            #print(str(rowCircleList) + " flushed")
+            rowCircleList = []
+            yCoordinateRowOfCircles = eachCircle[1]
+            rowCircleList.append(eachCircle)
+    rowCirclesSortedByX = sorted(rowCircleList, key = itemgetter(0))
+    fullySortedList = fullySortedList + rowCirclesSortedByX
+    #print(str(rowCircleList) + " flushed")
+#    print(fullySortedList)        
+    return fullySortedList
+
+def circlePixelID(circleList): # output pixel locations of all circles within the list,
+    circleIDpointer = 0
+    pixelLocations = []
+    for eachCircle in circleList:
+#        print("this circle is being analyzed in circle pixel ID")
+#        print(eachCircle)
+        xCoordCirc = eachCircle[0] # separates the x and y coordinates of the center of the circles and the circle radius 
+        yCoordCirc = eachCircle[1]
+        radiusCirc = eachCircle[2] + 2
+        for exesInCircle in range(( xCoordCirc - radiusCirc ),( xCoordCirc + radiusCirc )):
+            whyRange = np.sqrt(pow(radiusCirc,2) - pow((exesInCircle - xCoordCirc),2)) #calculates the y-coordinates that define the top and bottom bounds of a slice (at x position) of the circle 
+            discreteWhyRange = int(whyRange) 
+            for whysInCircle in range(( yCoordCirc - discreteWhyRange),( yCoordCirc + discreteWhyRange)):
+                pixelLocations.append([exesInCircle,whysInCircle, radiusCirc, circleIDpointer])
+        circleIDpointer = circleIDpointer + 1 
+    return pixelLocations
+
+def cvWindow(nameOfWindow, imageToShow, keypressBool):
+    print("----------Displaying: "
+          + str(nameOfWindow)
+          + "    ----------")
+    cv2.namedWindow(nameOfWindow, cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback(nameOfWindow, mouseLocationClick)
+    cv2.imshow(nameOfWindow, imageToShow)
+    pressedKey = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    if keypressBool:
+        return pressedKey
+
+    
+# read image to be analyzed
+rawImg = cv2.imread('slide1_14.tiff', 0)
+
+# read standard image
+standard_pattern = cv2.imread('standard_leptin_1.tiff', 0)
+stdWidth, stdHeight = standard_pattern.shape[::-1]
+
+#pattern match
+res = cv2.matchTemplate(rawImg,
+                        standard_pattern,
+                        cv2.TM_CCORR_NORMED)
+_, max_val, _, max_loc = cv2.minMaxLoc(res)
+
+#verify that the pattern was found
+
+bottomRightPt = (max_loc[0] + stdWidth,
+                 max_loc[1] + stdHeight)
+verImg = cv2.cvtColor(rawImg.copy(), cv2.COLOR_GRAY2RGB)
+cv2.rectangle(verImg, max_loc, bottomRightPt, (0, 0, 255), 2)
+cvWindow("verification", verImg, False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
