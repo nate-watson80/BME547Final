a318a7adb87ee16cbd228cab6193328dbd0463d7
Jason Cooper <jason.cooper.12@gmail.com>
2019-04-23 01:11:28 -0400

    Updated test_log_to_DB to now output a timestamp_id

--- test_log_to_DB.py
+++ test_log_to_DB.py
@@ -1,18 +1,17 @@
 import final_server as fs
+import pytest
 from pymongo import MongoClient
-from datetime import datetime
 
 db = fs.init_mongoDB()
 
 
 def test_log_to_DB():
-    timestamp = datetime.utcnow()
     log_data = {
                 "user": "testUser",
                 "client": "GUI-test",
                 "filename": "testFilename",
                }
     action = "Image Uploaded"
-    log_result = fs.log_to_DB(log_data, action)
-    exp_log_result = db.Logging.find_one({"timestamp": timestamp})["_id"]
-    assert log_result == exp_log_result
+    timestamp_id = fs.log_to_DB(log_data, action)
+    exp_timestamp_id = db.Logging.find_one({"_id": timestamp_id})["_id"]
+    assert timestamp_id == exp_timestamp_id
