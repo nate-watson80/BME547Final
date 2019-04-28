09a891bc4159ff946166d7f4b8c8e9d456a60450
Jason Cooper <jason.cooper.12@gmail.com>
2019-04-28 13:33:17 -0400

    Created test cases for circlePixelID

--- test_circlePixelID.py
+++ test_circlePixelID.py
@@ -1,4 +1,20 @@
 import final_server as fs
 import pytest
 
+
 def test_circlePixelID():
+    circleData = [5, 5, 1]
+    pixelLocations = fs.circlePixelID(circleData)
+    exp_pixelLocations = [[4, 5],
+                          [5, 4], [5, 5], [5, 6],
+                          [6, 5]]
+    assert pixelLocations == exp_pixelLocations
+
+    circleData = [5, 5, 2]
+    pixelLocations = fs.circlePixelID(circleData)
+    exp_pixelLocations = [[3, 5],
+                          [4, 4], [4, 5], [4, 6],
+                          [5, 3], [5, 4], [5, 5], [5, 6], [5, 7],
+                          [6, 4], [6, 5], [6, 6],
+                          [7, 5]]
+    assert pixelLocations == exp_pixelLocations
