046e5be3a727f7f4546904fe152e92cbd6fb1d44
Jason Cooper <jason.cooper.12@gmail.com>
2019-04-27 21:27:30 -0400

    Added expected cases for test_generatePatternMasks

--- test_generatePatternMasks.py
+++ test_generatePatternMasks.py
@@ -1,13 +1,32 @@
 import final_server as fs
 import pytest
-import base64
-import cv2
+
 
 def test_generatePatternMasks():
+    # Actual
     spot_info = [[138, 86, 29],
                  [136, 174, 29],
                  [136, 262, 29],
                  [134, 434, 29],
                  [134, 348, 28]]
     shape = [522, 244]
-    pattern, spotsMask, bgMask = fs.generatePatternMasks
+    pattern, spotsMask, bgMask = fs.generatePatternMasks(spot_info, shape)
+    pattern_test_array = [pattern[0, 0],
+                          pattern[100, 110],
+                          pattern[100, 125]]
+    spotsMask_test_array = [spotsMask[0, 0],
+                            spotsMask[100, 110],
+                            spotsMask[100, 125]]
+    bgMask_test_array = [bgMask[0, 0],
+                         bgMask[100, 110],
+                         bgMask[100, 125]]
+
+    # Expected
+    exp_pattern_test_array = [0, 100, 50]
+    exp_spotsMask_test_array = [0, 0, 255]
+    exp_bgMask_test_array = [255, 255, 0]
+
+    # Actual vs Expected
+    assert pattern_test_array == exp_pattern_test_array
+    assert spotsMask_test_array == exp_spotsMask_test_array
+    assert bgMask_test_array == exp_bgMask_test_array
